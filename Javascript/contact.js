fba5ac67dbd7f8e1d4d1fae262e326787314fcd8
k1383 <kantin1383@gmail.com>
2025-05-25 16:38:19 +0200

    form

--- Javascript/contact.js
+++ Javascript/contact.js
@@ -4,3 +4,25 @@ const burgermenu = document.querySelector("#burgermenu")
 burgermenu.addEventListener("click", function() {
     navlinks.classList.toggle("active")
 })
+
+/* Form */ 
+
+const myForm = document.getElementById('myForm')
+
+myForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const nameInput = document.getElementById('name')
+    const specialCharacters = /[0-9+=@*/%£¤&~{}<>:;?,!§]/;
+
+    //* .test() Permet de tester si un motif est présent
+
+    if (specialCharacters.test(nameInput.value)) {
+        alert("The name must not contain special characters.");
+    } if (nameInput.value.length < 3){
+        console.log("error");
+    } else {
+        alert("validé");
+    }
+})
+
